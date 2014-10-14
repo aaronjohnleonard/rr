d201c0447dcabd0d7ba27cf8d81074d8c68fe772
home <aaronHome@aaronl.dev.qualtrics.com>
2014-10-13 22:35:06 -0600

    fixed some things in textSender.js

--- textSender.js
+++ textSender.js
@@ -1,11 +1,36 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-   host		: 'us-cdbr-iron-east-01.cleardb.net',
+
+var db_config = {
+   host     : 'us-cdbr-iron-east-01.cleardb.net',
    user     : 'bf491532417c27',
    password : 'b2fff196'
-});
+};
 
-connection.query('USE reminders');
+var connection;
+
+function handleDisconnect() {
+    connection = mysql.createConnection(db_config);
+
+    connection.connect(function(err) {              // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+          console.log('error when connecting to db:', err);
+          setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });  
+
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+          handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+          throw err;                                  // server variable configures this)
+        }
+    });
+
+    connection.query('USE heroku_466f3f976236f66');
+}
+
+handleDisconnect();
 
 // Twilio Credentials 
 var accountSid = 'AC0884af1dd8f5d21d96239238558a056c'; 
