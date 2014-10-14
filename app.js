c1748224dda9bf47b5234d28b7beb1392f55e78f
home <aaronHome@aaronl.dev.qualtrics.com>
2014-10-13 22:01:21 -0600

    i tested it this time...

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('port', process.env.PORT || 5000);
 
 
 function handleDisconnect() {
-    connection = mysql.connection(db_config);
+    connection = mysql.createConnection(db_config);
 
     connection.connect(function(err) {              // The server is either down
         if(err) {                                     // or restarting (takes a while sometimes).
